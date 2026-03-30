f57308f8df8d53c027aad1c085de320193242b9e
Juan Sebastian <sebas313play@gmail.com>
2026-03-30 16:00:59 -0500

    feat: update branch

--- js/state.js
+++ js/state.js
@@ -1,43 +1,19 @@
-
 const state = {
-
     shows: [],
-
-
-    searchResults: [],
-
-
     currentPage: 1,
-    itemsPerPage: parseInt(localStorage.getItem('itemsPerPage')) || 20,
+    itemsPerPage: 20,
 
-    activeGenre: 'all',
-
-
-    favorites: JSON.parse(localStorage.getItem('favorites')) || [],
-    searchHistory: JSON.parse(localStorage.getItem('searchHistory')) || [],
-
-
-    setShows(newShows) {
-        this.shows = newShows;
+    setShows(data) {
+        this.shows = data;
     },
-
 
     setCurrentPage(page) {
         this.currentPage = page;
     },
 
-
-
-    setGenre(genre) {
-        this.activeGenre = genre;
-        this.currentPage = 1;
-    },
-
-
-    setItemsPerPage(count) {
-        this.itemsPerPage = count;
-        localStorage.setItem('itemsPerPage', count);
-        this.currentPage = 1;
+    setItemsPerPage(size) {
+        this.itemsPerPage = size;
+        localStorage.setItem("itemsPerPage", size);
     }
 };
 
